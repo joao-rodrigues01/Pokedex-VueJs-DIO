d13fb00de3416867ab614f1a64d0964646e19057
João Victor <j2003.victir@gmail.com>
2021-02-17 22:19:55 -0300

    right panel finished

---
+++ src/components/LeftPanel/LeftPanelContent.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="content">
+		<List />
+	</div>
+</template>
+
+<script>
+import List from '../List/List.vue';
+
+export default {
+	name: 'LeftPanelContent',
+	components: {
+		List,
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+	.content {
+		width: 100%;
+
+		@media (min-width: $viewport-medium) {
+			margin-top: 50px;
+		}
+	}
+
+</style>
